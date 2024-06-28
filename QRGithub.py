24d28ee542b5542eb218972ecd5b4d656e171b39
SakshiGadikar <94469552+SakshiGadikar@users.noreply.github.com>
2024-06-28 12:42:45 +0530

    Third Commit

---
+++ QRGithub.py
@@ -0,0 +1,21 @@
+import qrcode
+
+# URL to be converted to QR code
+url = "https://github.com/SakshiGadikar"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill='black', back_color='white')
+
+# Save the image to a file
+img.save("sakshi_github_qr.png")
+
